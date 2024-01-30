70dd7e18701ef755fc2117601b5e4e1c83b6e028
Carifio24 <carifio24@gmail.com>
2024-01-30 17:19:24 -0500

    WIP: Start creating colorbar.

---
+++ src/Colorbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="colorbar">
+  </div>
+</template>
+
+<script lang="ts">
+import { PropType, defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    colors: {
+      type: Array as PropType<[number, string][]>,
+      required: true
+    }
+  }
+});
+</script>
+
+<style lang="less">
+.colorbar {
+}
+</style>
