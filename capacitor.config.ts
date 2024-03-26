3a973f9351c7a5487465fd5f6c7e3333a786c6ba
Carifio24 <carifio24@gmail.com>
2024-03-26 18:41:49 -0400

    Install Capacitor plugins; create config.

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ds.cosmicds.solareclipse2024',
+  appName: 'solar-eclipse-2024',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
